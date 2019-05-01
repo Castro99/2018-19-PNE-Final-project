ab80f9906405a8f4fa13b5fafd58e343f6883bc5
Castro99 <david99esp@hotmail.es>
2019-05-01 18:21:16 +0200

    Python exercise

---
+++ Client.py
@@ -0,0 +1,15 @@
+#ADVANCE LEVEL
+#API FOTMATION
+import requests
+
+#For all the the URL(ENDPOINTS) in json
+HOST = "http://localhost:8000"
+URL = ["/listSpecies?limit=10&json=1", "/listSpecies?json=1", "karyotipe?specie=human&json=1"
+    "/karyotype?specie=mouse&json=1", "/chromosomeLength?specie=mouse&chromo=18&json=1", "/geneSeq?gene=FRAT1&json=1",
+    "/geneInfo?gene=FRAT1&json=1", "/geneCalc?gene=FRAT1&json=1", "/geneList?chromo=1&start=0&end=30000&json=1"]
+for i in URL:
+    link = HOST + i
+    r = requests.get(link, headers={"Content-Type": "application/json"})
+    json_code = r.json()
+
+    print(json_code)
