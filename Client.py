0298521f55043a0bc8b1c16046804069ba7a4f3a
Castro99 <david99esp@hotmail.es>
2019-05-02 14:57:46 +0200

    Not working properly

--- Client.py
+++ Client.py
@@ -7,8 +7,8 @@ host = "http://localhost:8000"
 URLS = ["/listSpecies?limit=10&json=1", "/listSpecies?json=1", "karyotipe?specie=human&json=1"
     "/karyotype?specie=mouse&json=1", "/chromosomeLength?specie=mouse&chromo=18&json=1", "/geneSeq?gene=FRAT1&json=1",
     "/geneInfo?gene=FRAT1&json=1", "/geneCalc?gene=FRAT1&json=1", "/geneList?chromo=1&start=0&end=30000&json=1"]
-for i in URLS:
-    link = host + i
+for l in URLS:
+    link = host + l
     r = requests.get(link, headers={"Content-Type": "application/json"})
     json_code = r.json()
 
