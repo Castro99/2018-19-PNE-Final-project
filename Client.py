c6fcd501f22aea4bcb37de5919c03ef3a53f78f4
Castro99 <david99esp@hotmail.es>
2019-05-06 20:56:18 +0200

    Not working properly

--- Client.py
+++ Client.py
@@ -4,9 +4,8 @@ import requests
 
 #For all the the URL(ENDPOINTS) in json
 host = "http://localhost:8000"
-URLS = ["/listSpecies?limit=10&json=1", "/listSpecies?json=1", "karyotipe?specie=human&json=1"
-    "/karyotype?specie=mouse&json=1", "/chromosomeLength?specie=mouse&chromo=18&json=1", "/geneSeq?gene=FRAT1&json=1",
-    "/geneInfo?gene=FRAT1&json=1", "/geneCalc?gene=FRAT1&json=1", "/geneList?chromo=1&start=0&end=30000&json=1"]
+URLS = ["/geneSeq?gene=FRAT1&json=1", "/geneInfo?gene=FRAT1&json=1", "/geneCalc?gene=FRAT1&json=1", "/geneList?chromo=1&start=0&end=30000&json=1", "/listSpecies?limit=10&json=1", "/listSpecies?json=1", "karyotipe?specie=human&json=1"
+    "/karyotype?specie=mouse&json=1", "/chromosomeLength?specie=mouse&chromo=18&json=1"]
 for l in URLS:
     link = host + l
     r = requests.get(link, headers={"Content-Type": "application/json"})
